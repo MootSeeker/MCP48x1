baa7767bfce20b68d99a426752b5fb1c24260af4
MootSeeker <mootseeker98@gmail.com>
2022-11-19 11:19:07 +0100

    modified:   include/mcp48x1.cpp

--- include/mcp48x1.cpp
+++ include/mcp48x1.cpp
@@ -1,15 +1,12 @@
 #include "mcp48x1.hpp"
 
 
-
-
-
 void mcp48x1::begin( uint8_t select )
 {
     _select = select; 
     pinMode( _select, OUTPUT );
     digitalWrite( _select, HIGH );
-    
+
 }
 
 void mcp48x1::begin( uint8_t select, uint8_t mosi, uint8_t sck )
